f2edb92045bdaeff964fd1f2cbd4fe14e09548c2
abrier23 <52480213+abrier23@users.noreply.github.com>
2019-07-04 15:13:55 -0700

    Add files via upload

---
+++ twitter_streaming.py
@@ -0,0 +1,29 @@
+#Import the necessary methods from tweepy library
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import twitter_cred as cred
+
+
+
+#This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print(data)
+        return True
+
+    def on_error(self, status):
+        print(status)
+
+
+if __name__ == '__main__':
+
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(cred.consumer_key, cred.consumer_secret)
+    auth.set_access_token(cred.access_token, cred.access_token_secret)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.filter(track=['python', 'javascript', 'ruby'])
