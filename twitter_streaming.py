2d2c9c4e64cb7b7b56f0c42aba59c11bfb75cc43
Aaron Brier <aj567br@gmail.com>
2019-07-04 21:16:18 -0700

    changes

--- twitter_streaming.py
+++ twitter_streaming.py
@@ -3,6 +3,7 @@
 #Import the necessary methods from tweepy library
 import tweepy
 import os
+import json
 
 consumer_key = os.environ['consumer_key']
 consumer_secret = os.environ['consumer_secret']
@@ -19,16 +20,19 @@ add to .bash_profile in home directory
 '''
 
 
-#This is a basic listener that prints tweets and saves to data.txt.
+#This is a basic listener that prints tweets and saves to raw_data.txt.
 class listener(tweepy.StreamListener):
 
-    def on_data(self, data):
-        print(data)
-        with open('data.txt','a') as thing:
-            thing.write(data)
+    def on_data(self, raw_data):
+        with open('raw_data.txt','a') as thing:
+            thing.write(raw_data)
+        data = json.loads(raw_data)
+        print(data['text'])
         return True
 
     def on_error(self, status):
+        if status == '420':
+            return False
         print(status)
 
 
@@ -40,9 +44,9 @@ if __name__ == '__main__':
     stream = tweepy.Stream(auth, listener()) #connects to twitter api
 
     #This captures input to filer tweets by
-    filters = []
-    while True:
-        user_input = input('Enter a keyword to filter tweets by (Type EXIT to stop): ')
-        if user_input == 'EXIT': break
-        else: filters.append(user_input)
+    filters = ['python']
+    # while True:
+    #     user_input = input('Enter a keyword to filter tweets by (Type EXIT to stop): ')
+    #     if user_input == 'EXIT': break
+    #     else: filters.append(user_input)
     stream.filter(track=filters)
